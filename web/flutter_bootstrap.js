3c91fd6a6d1d9ffe8f828ac60d3f0225db5814ff
daichangxia <>
2026-05-31 07:46:45 +0800

    bottom

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -1,5 +1,8 @@
 {{flutter_js}}
 {{flutter_build_config}}
 
-// 由 build_web_wasm.sh cdn 模式选用：skwasm 从 gstatic（engineRevision 见 _flutter.buildConfig）
-_flutter.loader.load();
+_flutter.loader.load({
+  config: {
+    canvasKitBaseUrl: '/canvaskit/',
+  },
+});
